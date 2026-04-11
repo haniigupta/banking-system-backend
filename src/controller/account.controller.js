8dba94a43867d9e95922dda93a17a33fc22e65b2
guptahani75@gmail.com <guptahani75@gmail.com>
2026-04-11 16:29:30 +0530

    ..

--- src/controller/account.controller.js
+++ src/controller/account.controller.js
@@ -1,5 +1,17 @@
 const accountModel = require('../models/account.model');
 
 async function createAccountController(req, res){
-    
+    const user = req.user;
+
+    const account = await accountModel.create({
+
+        user: user._id
+    })
+    return res.status(201).json({
+        message: 'Account created successfully',
+        account
+    })
+}
+module.exports = {
+    createAccountController
 }
