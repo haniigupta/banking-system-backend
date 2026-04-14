ff698af1eb3818c5e4b7703cf36fc96a3266578b
guptahani75@gmail.com <guptahani75@gmail.com>
2026-04-14 16:30:20 +0530

    made idempotentkeyt

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -7,5 +7,35 @@ const transactionSchema = new mongoose.Schema({
         ref: 'Account',
         required: [true, "Transaction must have a source account"],
         index: true,
-    }
-})
+    },
+    toAccount:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Account',
+        required: [true, "Transaction must have a destination account"],
+        index: true,
+    },
+    status:{
+        type: String,
+        enum:{
+            values: ['pending', 'completed', 'failed', 'reversed'],
+            message: 'Status must be either pending, completed, failed or reversed',
+        },
+        default: 'pending'
+    },
+    amount:{
+        type: Number,
+        required: [true, "Transaction must have an amount"],
+        min: [0, "Amount must be a positive number"]
+    },
+    // Unique key for idempotent requests to prevent duplicate transactions
+    // generate on client side
+    idempotencyKey:{
+        type: String,
+        required: [true, "Transaction must have an idempotency key"],
+        unique: true,
+        index: true,
+    },
+},{timestamps: true})
+
+const transactionModel = mongoose.model('Transaction', transactionSchema);
+module.exports = transactionModel;
